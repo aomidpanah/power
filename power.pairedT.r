5e674a584410e5c8f9321db51da3d775645f7227
Adam Omidpanah <aomidpanah@gmail.com>
2014-02-01 20:29:23 -0800

    Update power.pairedT.r
    Calculation of Cholesky (scaling) matrix generalized outside scope of replicated simulation section.

--- power.pairedT.r
+++ power.pairedT.r
@@ -1,8 +1,9 @@
 power.pairedT <- function(npair, rho, effectSize, sd, alpha, nsims=1000) {
+  Sigma <- matrix(c(1, rho, rho, 1), 2, 2)
+  cSig <- chol(Sigma) * sd
   out <- replicate(nsims, {
-    Sigma <- matrix(c(1, rho, rho, 1), 2, 2)
     Y <- matrix(rnorm(npair*2), npair, 2)
-    Y <- Y %*% chol(Sigma) * sd
+    Y <- Y %*% cSig
     Y[, 2] <- Y[, 2] + effectSize
     t.test(x=Y[, 1], y=Y[, 2], paired=TRUE)$p.value
   })
